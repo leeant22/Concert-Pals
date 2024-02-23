932a70b6d4525b11911accf6316bc91f344b4fae
leeant22 <leeant22@uw.edu>
2024-02-22 17:53:32 -0800

    Added hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,21 +4,28 @@ import { useState, useEffect } from 'react';
 
 export function NavBar() {
     const[menu, setMenu] = useState(false);
-    const [isWideScreen, setIsWideScreen] = useState(false);
+    const [wideScreen, setWideScreen] = useState(false);
+
     const toggleMenu = () => {
         setMenu(!menu);
     };
+
+    const handleClick = () => {
+        toggleMenu();
+    }
+
     useEffect(() => {
         const handleResize = () => {
-          setIsWideScreen(window.innerWidth > 640);
+          setWideScreen(window.innerWidth > 640);
         };
         window.addEventListener('resize', handleResize);
         handleResize();
         return () => window.removeEventListener('resize', handleResize);
       }, []);
+
     return (
         <div>
-            {isWideScreen ? (
+            {wideScreen ? (
                 <nav className="nav-bar">
                     <h1 className="title">Concert Pals</h1>
                     <ul>
@@ -31,16 +38,26 @@ export function NavBar() {
                 </nav>
             ) :
             (
-            <div>
-                <div><i className="fa-solid fa-bars"></i></div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/groups">Groups</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
-                    <li><Link to="/create-event" aria-label="Add An Event"><i className="fa-solid fa-plus"></i></Link></li>
-                    <li><Link to="/profile" aria-label="View Your Profile"><i className="fa-solid fa-circle-user"></i></Link></li>
-                </ul>
-            </div>
+            <nav className="nav-bar">
+                <div onClick={handleClick}>
+                    <i className={`hamburger ${menu ? "fas fa-times" : "fa-solid fa-bars"}`} />
+                </div>
+                <div className={`show-menu ${menu ? "visible" : "hide"}`}>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/groups">Groups</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                    </ul>
+                </div>
+                <div className='shift'>
+                </div>
+                <div className={!menu ? "show-icons" : "hide-icons"}>
+                    <ul>
+                        <li><Link to="/create-event" aria-label="Add An Event"><i className="fa-solid fa-plus"></i></Link></li>
+                        <li><Link to="/profile" aria-label="View Your Profile"><i className="fa-solid fa-circle-user"></i></Link></li>
+                    </ul>
+                </div>
+            </nav>
             )}
         </div>
     )
