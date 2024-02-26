52420f12e872b8cd3376737d9a70cfa148fd22b3
nawald <nawaldhabar@gmail.com>
2024-02-25 16:06:31 -0800

    modified event creation page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { NavBar } from './Nav.js';
 import { HomePage } from './Home.js';
 import { Profile } from './Profile.js';
+import { CreateEventPage } from './Create-Event.js';
 import { Route, Routes } from 'react-router-dom';
 
 function App(props) {
@@ -12,7 +13,8 @@ function App(props) {
                 <Route path="/" element={<HomePage data={props.events}/>}/>
                 <Route path="/groups" element={<HomePage data={props.events}/>}/>
                 <Route path="/contact" element={<HomePage data={props.events}/>}/>
-                <Route path="/create-event" element={<HomePage data={props.events}/>}/>
+                {/* <Route path="/create-event" element={<HomePage data={props.events}/>}/> */}
+                <Route path="/create-event" element={<CreateEventPage/>}/>
                 <Route path="/profile" element={<Profile/>}/>
             </Routes>
         </div>
---
+++ src/components/Create-Event.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Footer } from './Footer.js';
+
+export function CreateEventPage() {
+    const [eventName, setEventName] = useState("");
+    const [eventDate, setEventDate] = useState("");
+    const [eventLocation, setEventLocation] = useState("");
+    const [eventDesc, setEventDesc] = useState("");
+    const [submitForm, setSubmitForm] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Successfully submitted event:", { eventName, eventDate, eventLocation, eventDesc } );
+        setSubmitForm(true);
+    }
+
+    return (
+        <><div className="event-creation-page">
+            <main>
+            <h2 style={{ color: 'black' }}>Create Your Event</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="event-form-group">
+                    <label htmlFor="eventName">What is your event called?</label>
+                    <input type="text" id="eventName" value={eventName} onChange={event => setEventName(event.target.value)} required />
+                </div>
+                <div className="event-form-group">
+                    <label htmlFor="eventDate">When will your event take place?</label>
+                    <input type="date" id="eventDate" value={eventDate} onChange={event => setEventDate(event.target.value)} required />
+                </div>
+                <div className="event-form-group">
+                    <label htmlFor="eventLocation">Where will your event be located?</label>
+                    <input type="text" id="eventLocation" value={eventLocation} onChange={event => setEventLocation(event.target.value)} required />
+                </div>
+                <div className="event-form-group">
+                    <label htmlFor="eventDesc">Describe your event:</label>
+                    <textarea id="eventDesc" value={eventDesc} onChange={event => setEventDesc(event.target.value)} rows="8" required />
+                </div>
+                <button type="submit">Submit Your Event</button>
+            </form>
+            {submitForm && <p>Your event has been submitted!</p>}
+            </main>
+        </div>
+        <Footer /></>
+    )
+}
+
